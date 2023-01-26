ba7b3bf139b087d364cf17f2c7e8740f8f852e58
Medeiroshenrique <henrique.c.r.medeiros@gmail.com>
2023-01-26 16:11:52 -0300

    it's working

---
+++ Calculator.cs
@@ -0,0 +1,39 @@
+﻿namespace Math
+{
+    public class Calculator
+    {
+        private float FirstValue { get; set; }
+        private float SecondValue { get; set; }
+
+        public Calculator(float firstValue, float secondValue) {
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+        }
+
+        public float Addition(float firstValue, float secondValue)
+        {
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+            return firstValue + secondValue;
+        }
+        public float Multiplication(float firstValue, float secondValue)
+        {
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+            return firstValue * secondValue;
+        }
+        public float Subtraction(float firstValue, float secondValue)
+        {
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+            return FirstValue - secondValue;
+        }
+        public float Divide(float firstValue, float secondValue)
+        {
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+            return FirstValue / secondValue;
+        }
+
+    }
+}
---
+++ TestCalculator.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    public class TestCalculator
+    {
+        public static void Main() {
+            TestAddition();
+            TestMultiplication();
+            TestSubtraction();
+            TestDivide();
+        }
+
+        public static void TestAddition()
+        {
+            //Arrange
+            Calculator calculator = new Calculator(100, 200);
+            float DesiredResult = 300;
+           
+            //Act
+            float Result = calculator.Addition(100,200);
+            
+            //Assert
+            if (Result==DesiredResult) {
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine("Calculator->Addition Test-> OK");
+                Console.ResetColor();
+            }
+            else {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Calculator->Addition Test-> FAIL");
+                Console.ResetColor();
+            }
+
+        }
+
+        static void TestMultiplication() {
+            //Arrange
+            Calculator calculator = new Calculator(-150, 1200);
+            float DesiredResult = -180000;
+
+            //Act
+            float Result = calculator.Multiplication(-150,1200);
+
+            //Assert
+            if (Result == DesiredResult)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Calculator->Multiplication Test-> OK");
+                Console.ResetColor();
+            }
+            else {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Calculator->Multiplication Test-> FAIL");
+                Console.ResetColor();
+            }
+        }
+
+
+        static void TestSubtraction() {
+            //Arrange
+            Calculator calculator = new Calculator(-800, 1600);
+            float DesiredResult = -2400;
+
+            //Act
+            float Result= calculator.Subtraction(-800, 1600);
+
+            //Assert
+            if (Result==DesiredResult) {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Calculator->Subtraction Test-> OK");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Calculator->Subtraction Test-> FAIL");
+                Console.ResetColor();
+            }
+
+        }
+
+        static void TestDivide() {
+            //Arrange
+            Calculator calculator = new Calculator(-800, 200);
+            float DesiredResult = -4;
+
+            //Act
+            float Result = calculator.Divide(-800, 200);
+
+            //Assert
+            if (Result == DesiredResult)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Calculator->Divide Test-> OK");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Calculator->Divide Test-> FAIL");
+                Console.ResetColor();
+            }
+
+        }
+    }
+
+}
